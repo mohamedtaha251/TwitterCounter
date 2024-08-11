18c54231a8300a7f4454dcd296b821cb4306a483
mohamedtaha <m.ahmed.taha251@gmail.com>
2024-08-11 21:47:54 +0300

    remove unused theme code

--- app/src/main/java/com/example/twittercounter/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/twittercounter/ui/theme/MainActivity.kt
@@ -1,20 +1,16 @@
-package com.example.twittercounter
+package com.example.twittercounter.ui.theme
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.twittercounter.ui.theme.TwitterCharacterCount
-import com.example.twittercounter.ui.theme.TwitterCounterTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TwitterCounterTheme {
-                TwitterCharacterCount()
-            }
+            TwitterCharacterCount()
         }
     }
 }
